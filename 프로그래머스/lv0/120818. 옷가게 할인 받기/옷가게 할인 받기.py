66a8afb225985eb8378ab07479db028c5201f176
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2023-07-10 11:10:27 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    if price >= 500000:
+        result = int(price - price*0.2)
+    elif price>=300000:
+        result = int(price - price*0.1)
+    elif price >= 100000:
+        result = int(price - price*0.05)
+    else:
+        result = int(price)
+    return result
