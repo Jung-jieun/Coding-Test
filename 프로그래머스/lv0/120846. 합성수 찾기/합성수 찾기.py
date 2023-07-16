a00ffb81779f1acca0c3123e3b52c36daf5ed284
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2023-07-17 08:41:17 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,17 +1,11 @@
-def division(n):
-    result = 0
-    for i in range(1, n+1):
-        if n%i==0:
-            result += 1
-    return result
-
-def solution(k):
+def solution(n):
+    num = []
     answer = 0
-    for n in range(1, k+1):
-        if division(n)>2:
-            answer +=1
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            if i%j==0:
+                num.append(i)
+        if num.count(i)>=3:
+            answer += 1
     return answer
-
-    
             
-                
