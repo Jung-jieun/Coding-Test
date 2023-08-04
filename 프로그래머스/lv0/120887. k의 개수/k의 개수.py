bfdf416b629e1e1914d42d25fc358f8906d0b53f
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2023-08-04 13:15:03 +0900

    [level 0] Title: k의 개수, Time: 15.45 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.py
@@ -1,10 +1,3 @@
 def solution(i, j, k):
-    answer = 0
-    for num in range(i, j+1):
-        if str(k) == str(num):
-            answer += 1
-        elif str(k) in str(num):
-            for l in range(len(str(num))):
-                if str(num)[l] == str(k):
-                    answer += 1
+    answer = sum(str(i).count(str(k)) for i in range(i, j+1))
     return answer
