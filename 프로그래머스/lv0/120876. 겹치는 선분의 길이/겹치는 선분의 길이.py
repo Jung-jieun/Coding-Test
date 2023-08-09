3bf1ed4621bbbfdde7f8d9ba9330a2030d86d854
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2023-08-09 09:13:21 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,24 +1,3 @@
 def solution(lines):
-    num = []
-    for line in lines:
-        num += line
-    start, end = min(num), max(num)
-    
-    x,y,z = lines
-    answer = 0
-    
-    for i in range(start, end+1):
-        result = 0
-        
-        if x[0] <= i < x[1]:
-            result += 1
-            
-        if y[0] <= i < y[1]:
-            result += 1
-            
-        if z[0] <= i < z[1]:
-            result += 1
-        
-        if result >= 2: 
-            answer += 1
-    return answer
+    sets = [set(range(min(line), max(line))) for line in lines]
+    return len(sets[0]&sets[1] | sets[0]&sets[2] | sets[1]&sets[2])
