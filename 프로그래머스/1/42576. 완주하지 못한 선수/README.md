890ed959f11d3ffcb56cc529c3fd2ec91f1fa1be
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-03-01 15:54:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.65 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 26.2 MB, 시간: 70.36 ms
+메모리: 37.7 MB, 시간: 49.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 01일 15:21:31
+2024년 03월 01일 15:53:59
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,16 @@
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for i, j in zip(participant, completion):
-        if i!=j:
-            return i
-    return participant[-1]
+    hashdict = {}
+    sumhash = 0
+    
+    #1. hash : participant의 dictionary 만들기
+    for part in participant:
+        hashdict[hash(part)] = part # participant 이름
+        #2. participant의 sum(hash) 구하기
+        sumhash += hash(part) # hash한 값
+        
+    #3. completion의 sum(hash) 빼기
+    for comp in completion:
+        sumhash -= hash(comp)
+        
+    #4. 남은 값이 완주하지 못한 선수의 hash 값
+    return hashdict[sumhash]
