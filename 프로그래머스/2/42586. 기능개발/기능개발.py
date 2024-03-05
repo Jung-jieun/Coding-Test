445735c8941c9f714a71dbda3b7be0a6639c2ccf
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-03-05 12:37:24 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,19 @@
 def solution(progresses, speeds):
     answer = []
     
-    # 작업 시간 계산
-    days_remaining = [(100 - p) // s + ((100 - p) % s > 0) for p, s in zip(progresses, speeds)]
-    
-    # 배포 개수 계산
-    while days_remaining:
-        deploy_count = 1
-        days_to_deploy = days_remaining.pop(0)
+    while progresses: #비어 있으면 끝
+        cnt = 0 # 배포 수
         
-        # 동시에 배포될 수 있는 작업 개수 확인
-        while days_remaining and days_remaining[0] <= days_to_deploy:
-            deploy_count += 1
-            days_remaining.pop(0)
+        while progresses and progresses[0]>=100: 
+            # 리스트가 남아있고 맨 앞에 작업진도가 100이 된다면
+            cnt += 1
+            progresses.pop(0)
+            speeds.pop(0)
         
-        answer.append(deploy_count)
-    
+        progresses = [progresses[i] + speeds[i] for i in range(len(progresses))]
+        # progresses update.
+        
+        # 만약 오늘 배포된 기능 개수가 0이 아니라면 결과 리스트에 추가.
+        if cnt:
+            answer.append(cnt)
     return answer
