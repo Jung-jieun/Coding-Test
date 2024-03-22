b54ec6aacf4dbd11c02ca353d940e45d769ecce7
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-03-22 12:01:50 +0900

    [level 2] Title: 더 맵게, Time: 1760.94 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,22 +1,19 @@
 import heapq
-
 def solution(scoville, K):
     answer = 0
     h = []
     
-    # 모든 원소를 차례대로 힙에 삽입
     for value in scoville:
         heapq.heappush(h, value)
         
-    # 가장 작은 두 개의 음식을 섞어서 새로운 음식을 만들고 스코빌 지수를 확인하여 K 이상이 될 때까지 반복
-    while h[0] < K:
-        if len(h) < 2:
-            return -1  # 섞을 음식이 더 이상 없는 경우
+    while h[0]<K:
+        
+        if len(h)<2:
+            return -1
+        
         first = heapq.heappop(h)
         second = heapq.heappop(h)
-        new_scoville = first + (second * 2)
-        heapq.heappush(h, new_scoville)
+        new = first + (second*2)
+        heapq.heappush(h, new)
         answer += 1
-        
     return answer
-
