bc312b369fdc43647c48da38b59102b728645acf
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-03-26 08:46:34 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,21 @@
+from collections import deque
 def solution(progresses, speeds):
     answer = []
+    q = deque()
+    num = 0
     
-    while progresses: #비어 있으면 끝
-        cnt = 0 # 배포 수
-        
-        while progresses and progresses[0]>=100: 
-            # 리스트가 남아있고 맨 앞에 작업진도가 100이 된다면
-            cnt += 1
-            progresses.pop(0)
-            speeds.pop(0)
-        
-        progresses = [progresses[i] + speeds[i] for i in range(len(progresses))]
-        # progresses update.
-        
-        # 만약 오늘 배포된 기능 개수가 0이 아니라면 결과 리스트에 추가.
-        if cnt:
-            answer.append(cnt)
+    for i, j in zip(progresses, speeds):
+        if (100-i)%j==0:
+            q.append((100-i)//j)
+        else:
+            q.append((100-i)//j+1)
+    
+    while q:
+        work = q.popleft()
+        num += 1
+        while q and work>=q[0]:
+            num += 1
+            q.popleft()
+        answer.append(num)
+        num = 0
     return answer
