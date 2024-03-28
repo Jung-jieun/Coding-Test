7289febbb4e73b181c4254f4b976bb323e18f23c
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-03-29 08:51:35 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 139.16 ms, Memory: 43.4 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.py
@@ -0,0 +1,23 @@
+import heapq
+def solution(food_times, k):
+    
+    if sum(food_times)<=k:
+        return -1
+    
+    # 시간이 작은 음식부터 빼야 하므로 우선순위 큐를 이용
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i+1))
+        
+    sum_value = 0 # 먹기 위해 사용한 시간
+    previous = 0 # 직전에다 먹은 음식 시간 
+    length = len(food_times) # 남은 음식의 개수 
+    
+    while sum_value + ((q[0][0]-previous) * length) <= k:
+        now = heapq.heappop(q)[0]
+        sum_value += (now-previous) * length
+        length -= 1
+        previous = now
+        
+    result = sorted(q, key=lambda x:x[1]) # 음식의번호 기준으로 정렬 
+    return result[(k-sum_value) % length][1]
