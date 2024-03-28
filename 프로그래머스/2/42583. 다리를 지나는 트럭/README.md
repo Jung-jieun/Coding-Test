daedede43ebefaefa731479900b4d592dd04fd14
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-03-28 09:48:00 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 09일 08:19:43
+2024년 03월 28일 09:47:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,20 +1,23 @@
+from collections import deque
 def solution(bridge_length, weight, truck_weights):
-    time = 0
-    # 다리를 건너는 트럭
-    bridge = [0 for _ in range(bridge_length)]
-    bridge_weight = 0
     
-    while bridge :
-        time += 1
-        out_truck = bridge.pop(0)
-        bridge_weight -= out_truck
-        if truck_weights:
-            # bridge weight와 다음 truck의 무게가 weight를 넘지않으면 추가
-            if bridge_weight + truck_weights[0] <= weight: 
-                pass_truck = truck_weights.pop(0)
-                bridge.append(pass_truck)
-                bridge_weight += pass_truck
-            else:
-                bridge.append(0)
+    time = 0
+    bridge = deque([0] * bridge_length)
+    truck_weights = deque(truck_weights)
+    
+    currentWeight = 0
+    while len(truck_weights)!=0:
+        time+=1
+
+        currentWeight -= bridge.popleft()
+
+        if currentWeight + truck_weights[0] <= weight:
+            currentWeight+= truck_weights[0]
+            bridge.append(truck_weights.popleft())
+
+        else: 
+            bridge.append(0)
+            
+    time += bridge_length
     
     return time
