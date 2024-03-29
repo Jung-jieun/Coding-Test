83c0835fe7e28c947732b0782b6ccff3cb2ef628
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-03-29 13:26:32 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,26 +1,18 @@
 def solution(n, lost, reserve):
-    # 모든 학생이 1벌씩 가지고 있다고 가정
-    array = [1] * (n+1)
     
-    # 여벌의체육복 가져온 학생 
-    for r in reserve:
-        array[r] += 1
+    # 여벌 체육복을 가져왔는데 도난 당한 경우 제외 -> reserve랑 lost랑 중복 없애기
+    two = [r for r in reserve if r not in lost]
     
-    # 도난당한 학생
-    for l in lost:
-        array[l] -=1
+    # 체육복을 빌릴 수 있는 학생들 -> reserve랑 lost 중복 없애기
+    rent = [l for l in lost if l not in reserve]
     
-    # 체육복 빌려주기
-    for i in range(1, n+1):
-        if array[i]==0:
-        # 앞번호 학생이 여벌의 체육복이 있는 경우
-            if (i-1>=1) and (array[i-1]==2): 
-                array[i] += 1
-                array[i-1] -=1
-        # 뒷번호 학생이 여벌의 체육복이 있는 경우 
-            elif i+1 <=n and array[i+1]==2: 
-                array[i] += 1
-                array[i+1] -=1
+    two.sort()
+    rent.sort()
     
-    answer = sum(1 for a in array[1:] if a>0)
-    return answer
+    for idx in two:
+        if idx-1 in rent:
+            rent.remove(idx-1)
+        elif idx+1 in rent:
+            rent.remove(idx+1)
+    
+    return n-len(rent)
