74f3e88b10962f7a62a514c4b9fe20c99c52fba4
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-03-29 14:01:38 +0900

    [level 2] Title: 구명보트, Time: 7.10 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -2,12 +2,12 @@ def solution(people, limit):
     answer = 0
     people.sort()
     
-    light, heavy = 0, len(people)-1
+    light = 0
+    heavy = len(people)-1
     
-    while light <= heavy:
-        if people[light] + people[heavy] <=limit:
+    while light<heavy:
+        if people[light] + people[heavy] <= limit:
+            answer += 1
             light += 1
-        answer += 1
         heavy -= 1
-        
-    return answer
+    return len(people) - answer
