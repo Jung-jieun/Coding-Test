f99a713bd30879f9227bd736da8d6c099ae17980
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-08-05 10:13:58 +0900

    [level 1] Title: 문자열 나누기, Time: 1.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = 0
+    count1 = 0
+    count2 = 0
+    x = ""
+    
+    for i in s:
+        if count1==count2:
+            answer += 1
+            x = i 
+            count1 += 1
+        elif i==x:
+            count1 += 1
+        else:
+            count2 += 1
+    return answer
