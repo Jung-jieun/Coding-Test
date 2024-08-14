8a02f341e8ccd06e7ace5717c29fbdb7578c4ce2
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-08-14 09:06:44 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,13 +1,9 @@
 def solution(brown, yellow):
     answer = []
-    number = brown+yellow
     
-    for i in range(1, number):
-        if number%i==0:
-            if i>=(number//i):
-                answer.append([i,number//i])
-    
-    for array in answer:
-        if (array[0]-2)*(array[1]-2)==yellow:
-            return array
-    
+    for i in range(1, yellow+1):
+        if yellow%i == 0:
+            x = yellow/i
+            y = i
+            if 2*(x+y) + 4 == brown:
+                return [x+2, y+2]
