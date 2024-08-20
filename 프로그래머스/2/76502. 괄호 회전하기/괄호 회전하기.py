de00a612021c7ae5a8131d3fcd256e830c75e9f1
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-08-20 18:13:10 +0900

    [level 2] Title: 괄호 회전하기, Time: 280.82 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,25 @@
+from collections import deque
+def solution(s):
+    answer = 0
+    stack = []
+    s = deque(s)
+    
+    for _ in range(len(s)):
+        stack = []
+        for i in range(len(s)):
+            if len(stack)>0:
+                if stack[-1]=='(' and s[i]==')':
+                    stack.pop()
+                elif stack[-1]=='{' and s[i]=='}':
+                    stack.pop()
+                elif stack[-1]=='[' and s[i]==']':
+                    stack.pop()
+                else:
+                    stack.append(s[i])
+            else:
+                stack.append(s[i])                    
+        if len(stack)==0:
+            answer += 1
+        s.append(s.popleft())
+    
+    return answer
