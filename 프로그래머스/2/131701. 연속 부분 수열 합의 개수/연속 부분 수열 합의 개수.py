94780ea22e8349b762fb006bb865f7b91f7d7652
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-08-20 13:35:05 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4792.88 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,9 @@
+def solution(elements):
+    n = len(elements)
+    elements = elements*2
+    result = set()
+    
+    for i in range(n):
+        for j in range(n):
+            result.add(sum(elements[j:j+i+1]))
+    return len(result)
