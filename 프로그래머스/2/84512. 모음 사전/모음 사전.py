147295326e6d8b4abd3ff0cc51c28a5e6130b6f8
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-09-09 11:31:13 +0900

    [level 2] Title: 모음 사전, Time: 2.18 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,14 +1,15 @@
 def solution(word):
-    word_list = []
-    moeum = "AEIOU"
-    def dfs(length, alpha):
-        # 종료시점
-        if length==5:
-            return
+    answer = 0
+    string = 'AEIOU'
+    dictionary = []
+    
+    def dfs(cnt, w):
+        if cnt==5:
+            return 
         
-        for i in range(len(moeum)):
-            word_list.append(alpha+moeum[i])
-            dfs(length+1, alpha+moeum[i])
-            
-    dfs(0, "")
-    return word_list.index(word)+1
+        for s in string:
+            dictionary.append(w+s)
+            dfs(cnt+1, w+s)
+    
+    dfs(0, '')
+    return dictionary.index(word)+1
