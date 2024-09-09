f4ee863c78a694cc1b662f7c41ac3895633e4e67
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-09-09 11:01:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.89 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 5.15 ms
+메모리: 10.3 MB, 시간: 12.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 15일 10:34:55
+2024년 09월 09일 11:01:12
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,25 +1,31 @@
 from collections import deque
 def solution(maps):
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
+    answer = 0
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    queue = deque([(0,0)])
+    visited = [[False for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    visited[0][0] = True
+    
     n = len(maps)
     m = len(maps[0])
-    board = [] 
     
-    q = deque([(0,0,1)])
-    while q:
-        x, y, dist = q.popleft()
+    while queue:
+        x, y = queue.popleft()
         
         if x==n-1 and y==m-1:
-            return dist
+            answer = maps[x][y]
+            return answer
         
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
             
-            # 새로운 좌표가 범위 안에 있고, 벽이 아니라면 이동
-            if 0<=nx<n and 0<=ny<m and maps[nx][ny]== 1:
-                # 방문한 곳은 다시 방문하지 않도록 함
-                maps[nx][ny] = 0
-                q.append((nx,ny,dist+1))
-    return -1
+            if 0 <= nx < n and 0 <= ny < m:
+                if maps[nx][ny]!=0 and not visited[nx][ny]:
+                    maps[nx][ny] = maps[x][y] +1 
+                    queue.append((nx, ny))
+                    visited[nx][ny] = True
+    answer = -1
+    return answer
