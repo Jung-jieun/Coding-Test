4675c096a6d08de9b0b437e9a1321e9bb536d6a2
정지은 <106010232+Jung-jieun@users.noreply.github.com>
2024-09-23 08:53:33 +0900

    [level 2] Title: 소수 찾기, Time: 12.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,26 +1,20 @@
 from itertools import permutations
 def solution(numbers):
-    number = list(numbers)
-    num = []
+    answer = []
     
-    for i in range(1, len(number)+1):
-        for permutation in permutations(number,i):
-            if int(''.join(permutation))==1 or int(''.join(permutation))==0:
-                continue
-            else:
-                num.append(int(''.join(permutation)))
+    for i in range(1, len(numbers)+1):
+        p = permutations(numbers, i)
+        for j in p:
+            num = int(''.join(j))
+            
+            if num > 1:
+                is_prime = True
+                for k in range(2, int(num**0.5)+1):
+                    if num%k==0:
+                        is_prime = False
+                        break
+                
+                if is_prime and num not in answer:
+                    answer.append(num)
     
-    num = list(set(num))
-    answer = 0
-    
-    def is_prime(x):
-        for k in range(2, x):
-            if x%k==0:
-                return False
-        return True
-        
-    for j in num:
-        if is_prime(j):
-            answer += 1
-    
-    return answer
+    return len(answer)
